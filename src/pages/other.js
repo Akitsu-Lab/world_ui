e71d789af7119dca338f76a030d713527ba10446
SOTA <snssota@gmail.com>
2023-02-10 13:59:28 +0900

    🧑‍💻ページリンクを作成

--- src/pages/other.js
+++ src/pages/other.js
@@ -1,18 +1,19 @@
-import Link from "next/link";
 import Header from "./header";
+import Link from "next/link";
 
-export default function Home() {
-  let title = "Next.js Index";
+export default function Other() {
+  const title = "Other";
+
   return (
     <div>
       <Header title={title} />
       <h1 className="bg-primary px-3 text-white display-4">React</h1>
       <div className="container">
         <h3 className="my-3 text-primary text-center">{title}</h3>
-        <div className="card p-3 text-center">
-          <p>これは、ページ移動のサンプルです。</p>
-          <Link href="other" legacyBehavior>
-            <a>Go to Other page &gt;&gt;</a>
+        <div className="card p-3">
+          <p>これは、もう1つのページの表示です。</p>
+          <Link href="/" legacyBehavior>
+            <a>&lt;&lt; Black to Index page</a>
           </Link>
         </div>
       </div>
